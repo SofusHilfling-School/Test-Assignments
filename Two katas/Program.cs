627f7fde729b66f80673a23a1c683311fd3c31c6
sofushn <sofushn@hotmail.dk>
2021-09-09 10:27:28 +0200

    Added sln file and project file for Two kata assignment

---
+++ Two katas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Two_katas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
