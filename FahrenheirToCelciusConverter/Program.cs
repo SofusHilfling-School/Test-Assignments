c65cc028ed529251ec2c9123c0fecc9fe1badc42
sofushn <sofushn@hotmail.dk>
2021-09-11 18:39:30 +0200

    Implemented convertion from celcius to fahrenheit with unit tests.

--- FahrenheirToCelciusConverter/Program.cs
+++ FahrenheirToCelciusConverter/Program.cs
@@ -13,6 +13,19 @@ namespace FahrenheirToCelciusConverter
     public class TemperatureConverter
     {
         public double ToCelcius(double fahrenheirValue)
-            => (fahrenheirValue - 32.0d) / 1.8d;
+        {
+            if (fahrenheirValue < -459.67d)
+                throw new ArgumentOutOfRangeException(nameof(fahrenheirValue), "Cannot be below the value of absolute zero.");
+            return (fahrenheirValue - 32.0d) / 1.8d;
+        }
+
+        public double ToFahrenheit(double celciusValue)
+        {
+            if (celciusValue < -273.15d)
+                throw new ArgumentOutOfRangeException(nameof(celciusValue), "Cannot be below the value of absolute zero.");
+            else if (celciusValue > 9.98718408256842E+307)
+                throw new ArgumentOutOfRangeException(nameof(celciusValue), "Converted value is higher than the size of a double.");
+            return celciusValue * 1.8d + 32.0d;
+        }
     }
 }
--- FahrenheirToCelciusConverterTests/UnitTest.cs
+++ FahrenheirToCelciusConverterTests/UnitTest.cs
@@ -1,11 +1,22 @@
 using FahrenheirToCelciusConverter;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace FahrenheirToCelciusConverterTests
 {
     [TestClass]
     public class UnitTest
     {
+        [TestMethod]
+        public void ToCelcius_LowerThanAbsoluteZero_ThrowOutOfRangeException()
+        {
+            double temperatureInFahrenheir = -459.68d;
+            TemperatureConverter converter = new();
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(
+                () => converter.ToCelcius(temperatureInFahrenheir));
+        }
+
         [TestMethod]
         public void ToCelcius_AbsoluteZero_MatchExpectedCelciusValue()
         {
@@ -65,5 +76,96 @@ namespace FahrenheirToCelciusConverterTests
 
             Assert.AreEqual(expectedResult, celciusTempResult, 0.01);
         }
+
+        [TestMethod]
+        public void ToCelcius_DoubleMax_MatchExpectedCelciusValue()
+        {
+            double temperatureInFahrenheir = double.MaxValue;
+            double expectedResult = 9.98718408256842E+307;
+
+            TemperatureConverter converter = new();
+            double celciusTempResult = converter.ToCelcius(temperatureInFahrenheir);
+
+            Assert.AreEqual(expectedResult, celciusTempResult, 0.01);
+        }
+
+        [TestMethod]
+        public void ToFahrenheit_LowerThanAbsoluteZero_ThrowOutOfRangeException()
+        {
+            double temperatureInCelcius = -273.16d;
+            TemperatureConverter converter = new();
+            
+            Assert.ThrowsException<ArgumentOutOfRangeException>(
+                () => converter.ToFahrenheit(temperatureInCelcius));
+        }
+
+        [TestMethod]
+        public void ToFahrenheit_AbsoluteZero_MatchExpectedFahrenheitValue()
+        {
+            double temperatureInCelcius = -273.15d;
+            double expectedResult = -459.67d;
+            TemperatureConverter converter = new();
+
+            double fahrenheitTempResult = converter.ToFahrenheit(temperatureInCelcius);
+
+            Assert.AreEqual(expectedResult, fahrenheitTempResult, 0.01);
+        }
+
+        [TestMethod]
+        public void ToFahrenheit_WaterFrezzing_MatchExpectedFahrenheitValue()
+        {
+            double temperatureInCelcius = 0d;
+            double expectedResult = 32d;
+            TemperatureConverter converter = new();
+
+            double fahrenheitTempResult = converter.ToFahrenheit(temperatureInCelcius);
+
+            Assert.AreEqual(expectedResult, fahrenheitTempResult);
+        }
+        
+        [TestMethod]
+        public void ToFahrenheit_BoilingWater_MatchExpectedFahrenheitValue()
+        {
+            double temperatureInCelcius = 100d;
+            double expectedResult = 212d;
+            TemperatureConverter converter = new();
+
+            double fahrenheitTempResult = converter.ToFahrenheit(temperatureInCelcius);
+
+            Assert.AreEqual(expectedResult, fahrenheitTempResult);
+        }
+
+        [TestMethod]
+        public void ToFahrenheit_DoubleMax_ThrowOutOfRangeException()
+        {
+            double temperatureInCelcius = double.MaxValue;
+            TemperatureConverter converter = new();
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(
+                () => converter.ToFahrenheit(temperatureInCelcius));
+        }
+
+        [TestMethod]
+        public void ToFahrenheit_MaxCelciusValueSupported_MatchExpectedFahrenheitValue()
+        {
+            double temperatureInCelcius = 9.98718408256842E+307;
+            double expectedResult = double.MaxValue;
+            TemperatureConverter converter = new();
+
+            double fahrenheitTempResult = converter.ToFahrenheit(temperatureInCelcius);
+
+            Assert.AreEqual(expectedResult, fahrenheitTempResult);
+        }
+
+        [TestMethod]
+        public void ToFahrenheit_AboveMaxCelciusValueSupported_ThrowOutOfRangeException()
+        {
+            double temperatureInCelcius = 9.99E+307;
+            TemperatureConverter converter = new();
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(
+                () => converter.ToFahrenheit(temperatureInCelcius));
+        }
+
     }
 }
