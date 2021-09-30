47067c1e80284b9521bc339ea36c780c87c0b15b
cph-sn311 <cph-sn311@cphbusiness.dk>
2021-09-30 17:31:17 +0200

    Added string utilities and corresponding unit tests

---
+++ Assignment-2/StringUtilities/StringHelper.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Text;
+
+namespace StringUtilities
+{
+    public static class StringHelper
+    {
+        private static int ASCIILetterDelta = 'a' - 'A';
+
+        public static string Reverse(this string input)
+        {
+            _ = input ?? throw new ArgumentNullException(nameof(input));
+            if (input == string.Empty)
+                return input;
+
+            char[] output = new char[input.Length];
+            for (int outputIndex = 0, inputIndex = input.Length - 1; outputIndex < input.Length; outputIndex++, inputIndex--)
+            {
+                if (isMultipleSpanEmoji(inputIndex))
+                {
+                    output[outputIndex + 1] = input[inputIndex];
+                    output[outputIndex] = input[inputIndex - 1];
+                    outputIndex++;
+                    inputIndex--;
+                }
+                else
+                    output[outputIndex] = input[inputIndex];
+            }
+
+            return new string(output);
+
+            bool isMultipleSpanEmoji(int index)
+                => index > 0
+                    && input[index] >= 0xDC00 
+                    && input[index] <= 0xDFFF
+                    && input[index - 1] >= 0xD800 
+                    && input[index - 1] <= 0xDBFF;
+        }
+
+        public static string Upper(this string text)
+        {
+            _ = text ?? throw new ArgumentNullException(nameof(text));
+
+            StringBuilder result = new StringBuilder();
+            foreach(char c in text)
+            {
+                if ((c >= 'a' && c <= 'z') || c == 'æ' || c == 'ø' || c == 'å')
+                    result.Append((char)(c - ASCIILetterDelta));
+                else
+                    result.Append(c);
+            }
+            return result.ToString();
+        }
+
+        public static string Lower(this string text)
+        {
+            _ = text ?? throw new ArgumentNullException(nameof(text));
+
+            char[] outputChars = new char[text.Length];
+            for(int i = 0; i < text.Length; i++)
+            {
+                char c = text[i];
+                if ((c >= 'A' && c <= 'Z') || c == 'Æ' || c == 'Ø' || c == 'Å')
+                    outputChars[i] = (char)(c + ASCIILetterDelta);
+                else
+                    outputChars[i] = c;
+            }
+            return new string(outputChars);
+        }
+    }
+}
---
+++ Assignment-2/StringUtilitiesTests/StringHelperTests.cs
@@ -0,0 +1,149 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringUtilities;
+using System;
+
+namespace StringUtilitiesTests
+{
+    [TestClass]
+    public class StringHelperTests
+    {
+        [TestMethod]
+        public void Reverse_InputStringNull_ThrowsArgumentNullException()
+        {
+            string value = null;
+
+            Assert.ThrowsException<ArgumentNullException>(() => value.Reverse());
+        }
+
+        [TestMethod]
+        public void Reverse_WithSmallChars_StringIsReturnedInReverseOrder()
+        {
+            string value = "hello";
+            string expectedResult = "olleh";
+
+            string actualResult = value.Reverse();
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void Reverse_WithUpperChars_StringIsReturnedInReverseOrder()
+        {
+            string value = "UPPERCASE STING";
+            string expectedResult = "GNITS ESACREPPU";
+
+            string actualResult = value.Reverse();
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [DataTestMethod]
+        [DataRow("This is a test", "tset a si sihT")]
+        [DataRow("aBc", "cBa")]
+        public void Reverse_StringWithSmallAndLargeLetters_StringIsReturnedInReverseOrder(string value, string expectedResult)
+        {
+            string actualResult = value.Reverse();
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void Reverse_StringNumbers_StringIsReturnedInReverseOrder()
+        {
+            string value = "12fba aA";
+            string expectedResult = "Aa abf21";
+
+            string actualResult = value.Reverse();
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void Reverse_SpecialCharacters_StringIsReturnedInReverseOrder()
+        {
+            string value = "@$jk )@|";
+            string expectedResult = "|@) kj$@";
+
+            string actualResult = value.Reverse();
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void Reverse_SpecialLetters_StringIsReturnedInReverseOrder()
+        {
+            string value = "ø aådæa w";
+            string expectedResult = "w aædåa ø";
+
+            string actualResult = value.Reverse();
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void Reverse_StringWithEmoji_StringIsReturnedInReverseOrder()
+        {
+            string value = "❤🐨";
+            string expectedResult = "🐨❤";
+
+            string actualResult = value.Reverse();
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void Reverse_WithEscapeChar_StringIsReturnedInReverseOrder()
+        {
+            string value = "text with new\n line";
+            string expectedResult = "enil \nwen htiw txet";
+
+            string actualResult = value.Reverse();
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [DataTestMethod]
+        [DataRow("aBc", "ABC")]
+        [DataRow("this is a test", "THIS IS A TEST")]
+        [DataRow("12fba aA", "12FBA AA")]
+        [DataRow("@$jk )@|", "@$JK )@|")]
+        [DataRow("❤🐨", "❤🐨")]
+        [DataRow("text with new\n line", "TEXT WITH NEW\n LINE")]
+        public void Upper_Everything_ReturnStringWithAllLettersUppercase(string value, string expectedResult)
+        {
+            string actualResult = value.Upper();
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void Upper_InputStringNull_ThrowsArgumentNullException()
+        {
+            string value = null;
+
+            Assert.ThrowsException<ArgumentNullException>(() => value.Upper());
+        }
+
+        [DataTestMethod]
+        [DataRow("abc", "abc")]
+        [DataRow("this is a test", "this is a test")]
+        [DataRow("12fba aA", "12fba aa")]
+        [DataRow("@$jk )@|", "@$jk )@|")]
+        [DataRow("❤🐨", "❤🐨")]
+        [DataRow("text with new\n line", "text with new\n line")]
+        public void Lower_Everything_ReturnStringWithAllLettersUppercase(string value, string expectedResult)
+        {
+            string actualResult = value.Lower();
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void Lower_InputStringNull_ThrowsArgumentNullException()
+        {
+            string value = null;
+
+            Assert.ThrowsException<ArgumentNullException>(() => value.Upper());
+        }
+    }
+}
