5c3b7d291d96ea3f2963e0016aff9fea1f58ff8e
cph-sn311 <cph-sn311@cphbusiness.dk>
2021-10-29 22:57:04 +0200

    Added new employee service

---
+++ Assignment-3/BookingSystem/Services/EmployeeService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookingSystem.Models;
+using BookingSystem.Storage;
+
+namespace BookingSystem.Services
+{
+    public interface IEmployeeService
+    {
+        Employee GetEmployee(int employeeId);
+        int CreateEmployee(Employee employee);
+    }
+
+    public class EmployeeService: IEmployeeService
+    {
+        private readonly IEmployeeStorage _employeeStorage;
+
+        public EmployeeService(IEmployeeStorage employeeStorage)
+        {
+            _employeeStorage = employeeStorage;
+        }
+
+        public Employee GetEmployee(int employeeId)
+            => _employeeStorage.GetEmployee(employeeId);
+
+        public int CreateEmployee(Employee employee)
+            => _employeeStorage.CreateEmployee(employee);
+    }
+}
