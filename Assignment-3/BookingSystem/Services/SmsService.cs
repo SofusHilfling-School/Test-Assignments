94aa52649bd36f3bb966cbf6715651fb21f68b63
cph-sn311 <cph-sn311@cphbusiness.dk>
2021-10-29 17:23:13 +0200

    Addded SmsService

---
+++ Assignment-3/BookingSystem/Services/SmsService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookingSystem.Models;
+
+namespace BookingSystem.Services
+{
+    public interface SmsService
+    {
+        bool SendSms(SmsMessage message);
+    }
+}
